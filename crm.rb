25a1a35e608b278a761f7d9650f0ac7c9b13b101
Marlon O'Neil <hello@marlononeil.com>
2017-08-30 21:01:14 -0400

    added created new contact form to view

--- crm.rb
+++ crm.rb
@@ -19,6 +19,16 @@ get '/contacts/new' do
     erb :new
 end
 
+post '/contacts' do
+  Contact.create(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+  )
+  redirect to('/')
+end
+
 get '/contacts/:id' do
   @contact = Contact.find_by(id: params[:id].to_i)
   if @contact
